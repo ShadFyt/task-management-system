a7174df64dae5a418b2d2b6bcf2e86862f8e91b2
ShadFyt <doanryan10@gmail.com>
2025-09-17 22:00:30 -0400

    feat: add TypeORM database configuration and dependencies

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get(
+          'DB_PATH',
+          join(process.cwd(), 'apps/api/data/database.sqlite')
+        ),
+        entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
+        synchronize:
+          configService.get('NODE_ENV', 'development') !== 'production',
+        logging: configService.get('NODE_ENV') === 'development',
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
