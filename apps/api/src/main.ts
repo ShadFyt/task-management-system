f48ed743f6d724c9a206a88af9079e954413f104
ShadFyt <doanryan10@gmail.com>
2025-09-18 08:01:13 -0400

    feat: add class-validator and class-transformer with global validation pipe setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -19,6 +19,13 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, documentFactory);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
