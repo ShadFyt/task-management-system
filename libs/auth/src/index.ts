9a18f93de3133ee0a0afb34c546e5b3aa1ea7def
ShadFyt <doanryan10@gmail.com>
2025-09-21 16:52:31 -0400

    code clean up

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/auth';
-export * from './lib/auth.types';
 export * from './lib/rbac.utils';
 export * from './lib/rbac.types';
 export * from './lib/auth.schema';
--- libs/data/src/index.ts
+++ libs/data/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/data';
 export * from './lib/interfaces';
-export * from './lib/types/shared.types';
 export * from './lib/permission.schema';
 export * from './lib/role.schema';
 export * from './lib/user.schema';
