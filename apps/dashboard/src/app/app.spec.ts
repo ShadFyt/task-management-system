b7044378d5d65917e87bf2c2fcef7aae7845f107
ShadFyt <doanryan10@gmail.com>
2025-09-25 21:42:09 -0400

    add first tests for frontend

--- apps/dashboard/src/app/app.spec.ts
+++ apps/dashboard/src/app/app.spec.ts
@@ -9,12 +9,9 @@ describe('App', () => {
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should create the root component', () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome dashboard'
-    );
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
