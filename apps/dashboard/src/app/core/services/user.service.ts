11264be8e86440e62a017f4185337889e39adcf6
ShadFyt <doanryan10@gmail.com>
2025-10-07 14:23:57 -0400

    feat: add `UserService` for org-based user listing

---
+++ apps/dashboard/src/app/core/services/user.service.ts
@@ -0,0 +1,26 @@
+import { httpResource } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { API_BASE } from '../tokens';
+import { Store } from '@ngrx/store';
+import { selectSelectedOrgId } from '../../store';
+import { BareUser } from '@task-management-system/data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private readonly API_URL = inject(API_BASE);
+  private readonly store = inject(Store);
+  private readonly selectedOrgId = this.store.selectSignal(selectSelectedOrgId);
+
+  users = httpResource<BareUser[]>(
+    () => ({
+      url: `${this.API_URL}/users`,
+      params: {
+        orgId: this.selectedOrgId() ?? '',
+      },
+    }),
+    {}
+  );
+}
